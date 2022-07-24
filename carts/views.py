4d8f770166853749f28d0ebbd1c8c75cb00a6acc
Kunal Patil <kunalpatil970730@gmail.com>
2022-07-24 14:45:16 +0530

    commit

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,9 @@ from store.models import Product
 from carts.models import Cart, CartItem
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from carts.models import Cart, CartItem
+
 # Create your views here.
 
 def _cart_id(request):# _cart_id this is a just a private function. coding standard
@@ -42,8 +45,11 @@ def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
         grand_total = 0
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
@@ -80,6 +86,34 @@ def remove_cart_item(request, product_id):
     return redirect('cart')
 
 
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True) 
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        
+        tax = (2 * total)/100 
+        grand_total = total + tax 
+    except ObjectDoesNotExist:
+        pass # just ignore
+
+    context = {
+        'total'         : total,
+        'quantity'      : quantity,
+        'cart_items'    : cart_items,
+        'tax'           : tax,
+        'grand_total'   : grand_total,
+    }
+    return render(request, 'store/cart.html', context)
+
+@login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
